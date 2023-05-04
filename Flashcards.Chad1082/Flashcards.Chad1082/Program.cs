d95200fbad375ad591778a3b50816b3575b05dcd
Ian Chadburn <ian.chadburn@hotmail.co.uk>
2023-05-04 21:29:31 +0100

    Project Created

---
+++ Flashcards.Chad1082/Flashcards.Chad1082/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Flashcards.Chad1082
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
